7e8a799c46c7159dd7f12c54fc9e8d9f9334d375
rosee <rosee@tmon.co.kr>
2015-04-02 17:44:04 +0900

    add FE templates

---
+++ code snippets/front-ends/templates/tmpl.js
@@ -0,0 +1,35 @@
+/**
+ * Q'sTemplate @version 0.1
+ * 
+ * @author YiSeul Jo
+ *
+ * Copyright 2012 by YiSeul Jo
+ */
+
+(function () {
+"use strict";
+/*jslint eqeq: true, vars: true, devel: true */
+/*global $, console */
+
+var QsTemplate = function () {
+	/**
+	 * description
+	 * @param {}
+	 * 		* ...
+	 * @return {}
+	 */
+	function exec() {
+		
+	}
+};
+
+}());
+
+(function () {
+"use strict";
+/*jslint eqeq: true, vars: true, devel: true */
+/*global $, DBHandler, TWIS */
+
+
+
+}());
